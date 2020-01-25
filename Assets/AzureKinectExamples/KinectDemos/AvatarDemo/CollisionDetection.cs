a8041d0999dedf10d0d95574371f2862968939cc
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2020-01-26 00:43:14 +0900

    update

---
+++ Assets/AzureKinectExamples/KinectDemos/AvatarDemo/CollisionDetection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get Rigidbody
+        var rb = GetComponent<Rigidbody>();
+ 
+        // Freeze PositionXYZ and RotationXYZ of user
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        //rb.constraints = RigidbodyConstraints.FreezeAll;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/AzureKinectExamples/KinectDemos/AvatarDemo/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // スコア・ハイスコアを表示する
+        scoreText.text = score.ToString ();
+    }
+
+    private void Initialize ()
+    {
+        score = 0;
+    }
+
+    public void AddPoint (int point)
+    {
+        score += point;
+    }
+}
