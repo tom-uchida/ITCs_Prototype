b03bae75ee53ad6bb17eab5f388934545b29aee3
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2020-02-01 21:15:09 +0900

    apply effect

---
+++ Assets/Scripts/Emit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class Emit : MonoBehaviour
+{
+    [SerializeField] ParticleSystem[] ps;
+    [SerializeField] GameObject text;
+    [SerializeField] Vector2 textScale;
+    [SerializeField] float easingDuration;
+    bool isEmit = false;
+    
+
+    #region accessor
+    public bool IsEmit{
+        get{return isEmit;}
+        set{isEmit = value;}
+    }
+    #endregion
+    private void Awake() {
+        for(int i = 0; i < ps.Length; i++){
+            ps[i].playOnAwake = false;
+            ps[i].loop = false;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)){
+            isEmit = true;
+        }
+        if(isEmit){
+            foreach(var p in ps){
+                p.Play();
+                //text.transform.DOScale(new Vector3(textScale.x, textScale.y, 1.0f), 1.0f);
+                var seq = DOTween.Sequence();
+                seq.Append(text.transform.DOScale(new Vector3(textScale.x, textScale.y, 1.0f), easingDuration));
+                seq.Append(text.transform.DOScale(new Vector3(0.0f, 0.0f, 1.0f), easingDuration));
+                
+
+            }
+            
+        }
+        isEmit = false;
+    }
+}
